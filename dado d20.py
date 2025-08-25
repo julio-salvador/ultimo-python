f6e6cc0b555db19dd608b18b0010ce1d06103b1f
julio-salvador <julio.girardi@escola.pr.gov.br>
2025-08-25 14:13:50 +0000

    forca tutorial do mano Hashtag Programação

---
+++ dado d20.py
@@ -0,0 +1,16 @@
+
+palavra = "lando norris"
+letras_usuario = []
+chances = 4
+ganhou = False
+
+while True:
+
+    for letra in palavra:
+        if letra in palavra:
+        _
+   
+if ganhou:
+    print(f"Ganhou igual Verstappen em 2023. A palavra era: {palavra}")
+else:
+    print(f"Ai não irmão, ai tu tá errado! A palavra era: {palavra}")
