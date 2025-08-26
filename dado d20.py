f3bc029c9102961141afbb188a0e196d19ea8365
julio-salvador <julio.girardi@escola.pr.gov.br>
2025-08-26 12:18:23 +0000

    quase terminado

--- dado d20.py
+++ dado d20.py
@@ -1,16 +1,35 @@
 
-palavra = "lando norris"
+palavra = "mclaren"
 letras_usuario = []
-chances = 4
+chances = 6
 ganhou = False
 
 while True:
 
     for letra in palavra:
-        if letra in palavra:
-        _
-   
+        if letra.lower() in palavra_usuario:
+            print(letra, end= "")
+        else:
+
+            print("_", end= "")
+    print(f"Você tem {chances} chances")           
+    tentativa = input("Escolha uma letra para adivinhar: ")
+    letras_usuario.append(tentativa.lower())
+    if tentativa.lower() not in palavra.lower():
+        chances -= 1
+
+    ganhou = True
+    for letra in palavra:
+        if letra.lower() not in palavras_usuario:
+            ganhou = False
+
+
+    if chances == 0or ganhou:
+        break
+
+
+
 if ganhou:
-    print(f"Ganhou igual Verstappen em 2023. A palavra era: {palavra}")
+    print(f"ganhou. A palavra era: {palavra}")
 else:
     print(f"Ai não irmão, ai tu tá errado! A palavra era: {palavra}")
